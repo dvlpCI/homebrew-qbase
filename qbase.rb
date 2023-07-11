76ce25dd2dce8f00e0e2fcfbe78d03d65ceaae7c
dvlproad <dvlproad@163.com>
2023-07-11 16:56:06 +0800

    【Feature】init

---
+++ qbase.rb
@@ -0,0 +1,26 @@
+class Qbase < Formula
+  desc "QBase:脚本底层基础库"
+  homepage "https://github.com/dvlpCI/script-qbase"
+  url "https://github.com/dvlpCI/script-qbase/archive/0.0.1.tar.gz"
+  sha256 "35dd12c34060d0dc3f15289f29aafa70b836384e3a067c134cd8dcf1549681c6"
+  # shasum -a 256 xxxx.tar.gz
+  # version /(\d+\.\d+\.\d+)/  # 从 URL 中提取版本号
+
+  def install
+    # Install script to bin
+    # bin.install "helloworld.sh"
+    bin.install "qbase"
+    lib.install Dir["*"] # 记得添加lib目录，并将源码存放于lib里的src下
+  end
+
+
+  def uninstall
+    # rm "#{bin}/helloworld.sh"
+    rm "#{bin}/qbase"
+  end
+
+  test do
+    # Test your script
+    system "#{bin}/qbase", "--version"
+  end
+end
