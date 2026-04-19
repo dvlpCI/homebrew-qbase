caa4382d10e5e7148c46e46390adf0e585c88a49
dvlproad <dvlproad@163.com>
2026-04-20 01:15:47 +0800

    【Update】更新 qbase 到 0.9.12

--- qbase.rb
+++ qbase.rb
@@ -5,11 +5,11 @@ class Qbase < Formula
   desc "Qbase:脚本底层基础库"
   homepage "https://github.com/dvlpCI/script-qbase"
 
-  version "0.9.11"
+  version "0.9.12"
   # revision 1  # 版本号不变，但 revision 递增，会触发升级
   url "https://github.com/dvlpCI/script-qbase/archive/#{version}.tar.gz"
   # url "https://github.com/dvlpCI/script-qbase/archive/0.9.0.tar.gz"
-  sha256 "7db193164016a6041711178401045e47b4cc841f2c3d391d2cdf942581c382f7"
+  sha256 "9554685f3fa1b651d8e2444b43345d2154403ad1a9504518c6a6b0bccf792f16"
   # shasum -a 256 xxxx.tar.gz
   # version /(\d+\.\d+\.\d+)/  # 从 URL 中提取版本号
 
